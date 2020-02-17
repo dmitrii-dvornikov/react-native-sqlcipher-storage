41a1f8c78fe21dcfd767aa12f3ce47fb03874617
gillpeacegood <gill.peacegood@axsy.com>
2020-02-17 11:12:45 +0000

    work in progress windows

---
+++ windows/react-native-sqlcipher-storage/SqlCipherModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.ReactNative.Managed;
+using Newtonsoft.Json;
+
+namespace react_native_sqlcipher_storage
+{
+    [ReactModule]
+    public sealed class SqlCipherModule
+    {
+        [ReactMethod("open")]
+        public void open(JObject config)
+        {
+
+        }
+    }
+}
